947bed3dead0f59e3e71811c57f46f05ae5b0db9
panhang <46041767+sdtm66@users.noreply.github.com>
2021-06-18 13:26:53 +0800

    Add files via upload

---
+++ dev_os/boot_simpleos_print.asm
@@ -0,0 +1,22 @@
+print:
+    pusha
+start:
+    mov al,[bx]
+    cmp al,0
+    je done
+    mov ah,0x0e
+    int 0x10
+    add bx,1
+    jmp start
+done:
+    popa
+    ret
+print_nl:
+    pusha
+    mov ah,0x0e
+    mov al,0x0a;huan hang
+    int 0x10
+    mov al,0x0d;hui che
+    int 0x10
+    popa
+    ret
